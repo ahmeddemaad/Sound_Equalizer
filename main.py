e279b251037d26bbcc0c47e115cb8947b675b7f2
Ahmed Emad Eldeen Ahmed <ahmedemad9112@gmail.com>
2022-11-04 14:10:20 +0200

    removed generating button

--- main.py
+++ main.py
@@ -32,15 +32,15 @@ with tab1:
         amplitude,phase,rfrequency=fn.Fourier_operations(loaded_sound_file,sampling_rate)
         # rfrequency,amplitude= fn.magnitude_spectrum_ (amplitude,sampling_rate, 1 )
         ax = plt.figure(figsize=(10, 8))
-        List_freq_axis, List_amplitude_axis=fn.bins_separation(rfrequency, amplitude)
-        sliders_date=fn.Sliders_generation()
+        List_freq_axis, List_amplitude_axis,bin_max_frequency_value=fn.bins_separation(rfrequency, amplitude)
+        sliders_date=fn.Sliders_generation(bin_max_frequency_value)
         mod_List_amplitude_axis,empty=fn.sound_modification(sliders_date,List_amplitude_axis)
         phase=phase[:len(mod_List_amplitude_axis):1]
         ifft_file=fn.inverse_fourier(mod_List_amplitude_axis,phase)    # generate = st.button('Generate')
-        generate=st.button('Generate')
-        if generate:
-            song=ipd.Audio(ifft_file,rate=sampling_rate/2)
-            empty.write(song)
+        # generate=st.button('Generate')
+        # if generate:
+        song=ipd.Audio(ifft_file,rate=sampling_rate/2)
+        empty.write(song)
         rfrequency=rfrequency[:len(mod_List_amplitude_axis):1]
         # plt.plot(rfrequency, mod_List_amplitude_axis, color='black')
         # st.plotly_chart(ax)
